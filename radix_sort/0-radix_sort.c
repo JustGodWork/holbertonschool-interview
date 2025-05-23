a729190a72dab6d56366e169489b53c51334f765
JustGodWork <discord1elie@gmail.com>
2025-05-23 21:49:11 +0200

    Refactor radix sort functions for improved readability and consistency

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -9,15 +9,15 @@
  */
 static int get_max(int *array, size_t size)
 {
-    size_t i;
-    int max = array[0];
+	size_t i;
+	int max = array[0];
 
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-    return (max);
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
 }
 
 /**
@@ -28,29 +28,29 @@ static int get_max(int *array, size_t size)
  */
 static void counting_sort_radix(int *array, size_t size, int exp)
 {
-    int *output, count[10] = {0};
-    size_t i;
+	int *output, count[10] = {0};
+	size_t i;
 
-    output = malloc(sizeof(int) * size);
-    if (!output)
-        return;
+	output = malloc(sizeof(int) * size);
+	if (!output)
+		return;
 
-    for (i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
 
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
 
-    for (i = size; i > 0; i--)
-    {
-        output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
-        count[(array[i - 1] / exp) % 10]--;
-    }
+	for (i = size; i > 0; i--)
+	{
+		output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
+		count[(array[i - 1] / exp) % 10]--;
+	}
 
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-    free(output);
+	free(output);
 }
 
 /**
@@ -60,15 +60,15 @@ static void counting_sort_radix(int *array, size_t size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max, exp;
+	int max, exp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    max = get_max(array, size);
-    for (exp = 1; max / exp > 0; exp *= 10)
-    {
-        counting_sort_radix(array, size, exp);
-        print_array(array, size);
-    }
+	max = get_max(array, size);
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_sort_radix(array, size, exp);
+		print_array(array, size);
+	}
 }
