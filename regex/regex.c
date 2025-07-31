b1373719eb14fc0a63a39159538fa65d719861b4
JustGodWork <discord1elie@gmail.com>
2025-07-31 16:10:53 +0200

    Refactor regex_match function for consistent indentation

--- regex/regex.c
+++ regex/regex.c
@@ -9,20 +9,20 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-    if (!pattern || !str)
-        return (0);
-    if (*pattern == '\0')
-        return (*str == '\0');
-    if (*(pattern + 1) == '*')
-    {
-        if (regex_match(str, pattern + 2))
-            return (1);
-        if ((*str && (*pattern == *str || *pattern == '.')) &&
-            regex_match(str + 1, pattern))
-            return (1);
-        return (0);
-    }
-    if (*str && (*pattern == *str || *pattern == '.'))
-        return (regex_match(str + 1, pattern + 1));
-    return (0);
+	if (!pattern || !str)
+		return (0);
+	if (*pattern == '\0')
+		return (*str == '\0');
+	if (*(pattern + 1) == '*')
+	{
+		if (regex_match(str, pattern + 2))
+			return (1);
+		if ((*str && (*pattern == *str || *pattern == '.')) &&
+			regex_match(str + 1, pattern))
+			return (1);
+		return (0);
+	}
+	if (*str && (*pattern == *str || *pattern == '.'))
+		return (regex_match(str + 1, pattern + 1));
+	return (0);
 }
