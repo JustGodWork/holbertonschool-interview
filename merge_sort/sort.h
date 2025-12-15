d1d17046cd0cf88399fdb5e767f65223d1ba2d8a
JustGodWork <discord1elie@gmail.com>
2025-12-15 11:58:42 +0100

    Implement merge sort algorithm with auxiliary functions and update README

---
+++ merge_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+
+/* Prototype for provided helper that prints arrays */
+void print_array(const int *array, size_t size);
+
+/* Prototype for merge sort implementation */
+void merge_sort(int *array, size_t size);
+
+#endif /* SORT_H */
